90699c705cc2553a9298f523dc26257560bcdf25
Lab09_Weronika_Gaj <weronika.gaj@student.pk.edu.pl>
2026-06-12 19:39:07 +0200

    Dodanie konfiguracji testów i pokrycia kodu

--- backend/src/main/java/edu_gaj/pasir_gaj_weronika/PasirGajWeronikaApplication.java
+++ backend/src/main/java/edu_gaj/pasir_gaj_weronika/PasirGajWeronikaApplication.java
@@ -1,13 +1,12 @@
 package edu_gaj.pasir_gaj_weronika;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootApplication
-public class PasirGajWeronikaApplication {
+@SpringBootTest
+class PasirGajWeronikaApplicationTests {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PasirGajWeronikaApplication.class, args);
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }

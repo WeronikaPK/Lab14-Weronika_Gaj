e468a5cf8e54cb3049652d1a4f964d540537bed2
Lab09_Weronika_Gaj <weronika.gaj@student.pk.edu.pl>
2026-06-12 19:47:40 +0200

    Dodanie zaleznosci do testow Spring Boot

--- backend/src/main/java/edu_gaj/pasir_gaj_weronika/PasirGajWeronikaApplication.java
+++ backend/src/main/java/edu_gaj/pasir_gaj_weronika/PasirGajWeronikaApplication.java
@@ -1,5 +1,17 @@
 package edu_gaj.pasir_gaj_weronika;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasirGajWeronikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasirGajWeronikaApplication.class, args);
+	}
+
+}
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 

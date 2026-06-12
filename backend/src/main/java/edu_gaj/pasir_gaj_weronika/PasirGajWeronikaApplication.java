3414589be91bd11595926d8c1b1d1aaba8dc273a
Lab09_Weronika_Gaj <weronika.gaj@student.pk.edu.pl>
2026-06-12 19:52:29 +0200

    Naprawa klasy glownej aplikacji

--- backend/src/main/java/edu_gaj/pasir_gaj_weronika/PasirGajWeronikaApplication.java
+++ backend/src/main/java/edu_gaj/pasir_gaj_weronika/PasirGajWeronikaApplication.java
@@ -6,19 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PasirGajWeronikaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PasirGajWeronikaApplication.class, args);
-	}
-
-}
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class PasirGajWeronikaApplicationTests {
-
-    @Test
-    void contextLoads() {
+    public static void main(String[] args) {
+        SpringApplication.run(PasirGajWeronikaApplication.class, args);
     }
 }
